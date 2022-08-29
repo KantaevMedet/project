a5c0a3b4dde5b28c3f09d5971a7c2819c4df66f2
Medet <k-medet@mail.ru>
2022-08-29 23:20:00 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/* let number = 1;
+const leftBorderWight = 1;
+
+console.log(leftBorderWight);
+
+const obj = {
+    name: "Jonh",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bnp'];
+console.log(arr.length);
+
+const store = "Amanat";
+let storeDescription = {
+    budget: 10000,
+    employees: ["Andrey", "Maxim", "Pavel"],
+    products: {
+        table: 2000,
+        chairs: 500
+    },
+    open: true
+};
+//storeDescription.employees = ["Andrey", "Maxim", "Pavel"];
+console.log(storeDescription); */
+
+//const result = confirm("че там ?");
+//console.log(result);
+//const answer = +prompt("вам есть 18");
+//console.log(typeof(answer));
+
+// const answers =[];
+// answers[0] = prompt("Укажите ваше имя",'');
+// answers[1] = prompt("Укажите ваше фамилию",'');
+// answers[2] = prompt("Укажите ваш возраст",'');
+
+// document.write(answers);
+
+let incr = 10;
+let decr = 10;
+console.log(++incr);
+console.log(--decr);
+
+console.log(2+2*2 !== '6');
+const isChecked = true;
+const isClosed = false;
+
+console.log(isChecked || !isClosed);
